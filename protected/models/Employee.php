87ca69a7193bbcd793b21b0d0b01add76dc1634d
Filip Slavik <filippos@slavik.gr>
2014-06-10 21:41:23 +0300

    added employee model

---
+++ protected/models/Employee.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * This is the model class for table "employee".
+ *
+ * The followings are the available columns in table 'employee':
+ * @property integer $am
+ * @property string $name
+ * @property string $surname
+ * @property string $afm
+ * @property integer $school_id
+ *
+ * The followings are the available model relations:
+ * @property Secondment[] $secondments
+ */
+class Employee extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'employee';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('am, name, surname', 'required'),
+			array('am, school_id', 'numerical', 'integerOnly'=>true),
+			array('name', 'length', 'max'=>30),
+			array('surname', 'length', 'max'=>60),
+			array('afm', 'length', 'max'=>10),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('am, name, surname, afm, school_id', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'secondments' => array(self::HAS_MANY, 'Secondment', 'employee_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'am' => 'Am',
+			'name' => 'Name',
+			'surname' => 'Surname',
+			'afm' => 'Afm',
+			'school_id' => 'School',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('am',$this->am);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('surname',$this->surname,true);
+		$criteria->compare('afm',$this->afm,true);
+		$criteria->compare('school_id',$this->school_id);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Employee the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}

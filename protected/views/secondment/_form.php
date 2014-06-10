b5a9815d63e704de5b9145634896c85cf4b6ede5
Filip Slavik <filippos@slavik.gr>
2014-06-10 21:26:40 +0300

    added bootstrap

--- protected/views/secondment/_form.php
+++ protected/views/secondment/_form.php
@@ -4,132 +4,115 @@
 /* @var $form CActiveForm */
 ?>
 
-<div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'secondment-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
+<?php /** @var BootActiveForm $form */
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id'=>'secondment-form',
+    'type'=>'vertical',
+		'htmlOptions'=>array('class'=>'well')
 )); ?>
 
-	<p class="note">Τα πεδία μαρκαρισμένα με <span class="required">*</span> είναι υποχρεωτικά.</p>
-
-	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'oik_katastasi'); ?>
-		<!--
-		<?php echo $form->textField($model,'oik_katastasi',array('size'=>60,'maxlength'=>60)); ?>
-		-->
-		<?php echo $form->dropdownList($model,'oik_katastasi',array(
+<fieldset>
+	<legend>Οικογενειακή Κατάσταση</legend>
+		<div class="row">
+			<div class="col-md-1">
+		<?php echo $form->dropdownListRow($model,'oik_katastasi',array(
 			'ΑΓΓΑΜΟΣ'=>'Άγγαμος',
 			'ΕΓΓΑΜΟΣ'=>'Έγγαμος',
 			'ΣΕ ΧΗΡΕΙΑ'=>'Σε χηρεία',
 			'ΣΕ ΧΗΡΕΙΑ ΜΕ ΑΝΗΛΙΚΟ'=>'Σε χηρεία με ανήλικο',
-			'ΜΟΝΟΓΟΝΕΪΚΗ'=>'Μονογονεϊκη',
-		)); ?>
+			'ΜΟΝΟΓΟΝΕΪΚΗ'=>'Μονογονεϊκη'), array('hint'=>'Δηλώστε την οικογενειακή σας κατάσταση')); ?>
 		<?php echo $form->error($model,'oik_katastasi'); ?>
-	</div>
+		</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'paidia'); ?>
-		<?php echo $form->numberField($model,'paidia', array('size'=>2,'maxlength'=>2)); ?>
+		<div class="col-md-1">
+		<?php echo $form->textFieldRow($model,'paidia', array('size'=>2,'maxlength'=>2)); ?>
 		<?php echo $form->error($model,'paidia'); ?>
 	</div>
+	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'eksosomatiki'); ?>
-		<?php echo $form->checkBox($model,'eksosomatiki'); ?>
+		<legend>Λόγοι Υγείας</legend>
+
+
+		<?php echo $form->checkBoxRow($model,'eksosomatiki'); ?>
 		<?php echo $form->error($model,'eksosomatiki'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'ygeia_idios'); ?>
-		<!--
-		<?php echo $form->textField($model,'ygeia_idios',array('size'=>15,'maxlength'=>15)); ?>
-		-->
-		<?php echo $form->dropdownList($model,'ygeia_idios',array(
-			'' => 'Επιλέξτε ποσοστό',
-			'50-66%'=>'50% με 66%',
-			'67-79%'=>'67% με 79%',
-			'80% κ άνω'=>'80% και άνω',
-		)); ?>
-		<?php echo $form->error($model,'ygeia_idios'); ?>
-	</div>
+			<?php echo $form->dropdownListRow($model,'ygeia_idios',array(
+				'' => 'Επιλέξτε ποσοστό',
+				'50-66%'=>'50% με 66%',
+				'67-79%'=>'67% με 79%',
+				'80% κ άνω'=>'80% και άνω',
+			)); ?>
+			<?php echo $form->error($model,'ygeia_idios'); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'ygeia_syzigos'); ?>
-		<!-- <?php echo $form->textField($model,'ygeia_syzigos',array('size'=>15,'maxlength'=>15)); ?> -->
-		<?php echo $form->dropdownList($model,'ygeia_syzigos',array(
-			'' => 'Επιλέξτε ποσοστό',
-			'50-66%'=>'50% με 66%',
-			'67-79%'=>'67% με 79%',
-			'80% κ άνω'=>'80% και άνω',
-		)); ?>
-		<?php echo $form->error($model,'ygeia_syzigos'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'ygeia_paidi'); ?>
-		<!-- <?php echo $form->textField($model,'ygeia_paidi',array('size'=>15,'maxlength'=>15)); ?> -->
-		<?php echo $form->dropdownList($model,'ygeia_paidi',array(
-			'' => 'Επιλέξτε ποσοστό',
-			'50-66%'=>'50% με 66%',
-			'67-79%'=>'67% με 79%',
-			'80% κ άνω'=>'80% και άνω',
-		)); ?>
-		<?php echo $form->error($model,'ygeia_paidi'); ?>
-	</div>
+			<?php echo $form->labelEx($model,'ygeia_syzigos'); ?>
+			<!-- <?php echo $form->textField($model,'ygeia_syzigos',array('size'=>15,'maxlength'=>15)); ?> -->
+			<?php echo $form->dropdownList($model,'ygeia_syzigos',array(
+				'' => 'Επιλέξτε ποσοστό',
+				'50-66%'=>'50% με 66%',
+				'67-79%'=>'67% με 79%',
+				'80% κ άνω'=>'80% και άνω',
+			)); ?>
+			<?php echo $form->error($model,'ygeia_syzigos'); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'ygeia_adelfos'); ?>
-		<!-- <?php echo $form->textField($model,'ygeia_adelfos',array('size'=>15,'maxlength'=>15)); ?> -->
-		<?php echo $form->dropdownList($model,'ygeia_adelfos',array(
-			'' => 'Επιλέξτε ποσοστό',
-			'67% κ άνω'=>'67% και άνω',
-		)); ?>
-		<?php echo $form->error($model,'ygeia_adelfos'); ?>
-	</div>
+			<?php echo $form->labelEx($model,'ygeia_paidi'); ?>
+			<!-- <?php echo $form->textField($model,'ygeia_paidi',array('size'=>15,'maxlength'=>15)); ?> -->
+			<?php echo $form->dropdownList($model,'ygeia_paidi',array(
+				'' => 'Επιλέξτε ποσοστό',
+				'50-66%'=>'50% με 66%',
+				'67-79%'=>'67% με 79%',
+				'80% κ άνω'=>'80% και άνω',
+			)); ?>
+			<?php echo $form->error($model,'ygeia_paidi'); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'entopiotita'); ?>
-		<!-- <?php echo $form->textField($model,'entopiotita',array('size'=>15,'maxlength'=>15)); ?> -->
-		<?php echo $form->dropdownList($model,'entopiotita',array(
-			'' => 'Επιλέξτε Δήμο',
-			'ΗΡΑΚΛΕΙΟΥ'=>'Ηρακλείου',
-			'ΜΑΛΕΒΙΖΙΟΥ'=>'Μαλεβιζίου',
-			'ΧΕΡΣΟΝΗΣΟΥ'=>'Χερσονήσου',
-			'ΑΡΧΑΝΩΝ-ΑΣ.'=>'Αρχανών - Αστερουσίων',
-			'ΜΙΝΩΑ-ΠΕΔ.'=>'Μίνωα - Πεδιάδος',
-			'ΦΑΙΣΤΟΥ.'=>'Φαιστού',
-			'ΓΟΡΤΥΝΑΣ'=>'Γόρτυνας',
-			'ΒΙΑΝΝΟΥ'=>'Βιάννου',
-		)); ?>
+			<?php echo $form->labelEx($model,'ygeia_adelfos'); ?>
+			<!-- <?php echo $form->textField($model,'ygeia_adelfos',array('size'=>15,'maxlength'=>15)); ?> -->
+			<?php echo $form->dropdownList($model,'ygeia_adelfos',array(
+				'' => 'Επιλέξτε ποσοστό',
+				'67% κ άνω'=>'67% και άνω',
+			)); ?>
+			<?php echo $form->error($model,'ygeia_adelfos'); ?>
 
-		<?php echo $form->error($model,'entopiotita'); ?>
-	</div>
+	<legend>Εντοπιότητα</legend>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'synyphrethsh'); ?>
-		<!-- <?php echo $form->textField($model,'synyphrethsh',array('size'=>15,'maxlength'=>15)); ?> -->
-		<?php echo $form->dropdownList($model,'synyphrethsh',array(
-			'' => 'Επιλέξτε Δήμο',
-			'ΗΡΑΚΛΕΙΟΥ'=>'Ηρακλείου',
-			'ΜΑΛΕΒΙΖΙΟΥ'=>'Μαλεβιζίου',
-			'ΧΕΡΣΟΝΗΣΟΥ'=>'Χερσονήσου',
-			'ΑΡΧΑΝΩΝ-ΑΣ.'=>'Αρχανών - Αστερουσίων',
-			'ΜΙΝΩΑ-ΠΕΔ.'=>'Μίνωα - Πεδιάδος',
-			'ΦΑΙΣΤΟΥ.'=>'Φαιστού',
-			'ΓΟΡΤΥΝΑΣ'=>'Γόρτυνας',
-			'ΒΙΑΝΝΟΥ'=>'Βιάννου',
-		)); ?>
-		<?php echo $form->error($model,'synyphrethsh'); ?>
-	</div>
+	<?php echo $form->labelEx($model,'entopiotita'); ?>
+	<!-- <?php echo $form->textField($model,'entopiotita',array('size'=>15,'maxlength'=>15)); ?> -->
+	<?php echo $form->dropdownList($model,'entopiotita',array(
+		'' => 'Επιλέξτε Δήμο',
+		'ΗΡΑΚΛΕΙΟΥ'=>'Ηρακλείου',
+		'ΜΑΛΕΒΙΖΙΟΥ'=>'Μαλεβιζίου',
+		'ΧΕΡΣΟΝΗΣΟΥ'=>'Χερσονήσου',
+		'ΑΡΧΑΝΩΝ-ΑΣ.'=>'Αρχανών - Αστερουσίων',
+		'ΜΙΝΩΑ-ΠΕΔ.'=>'Μίνωα - Πεδιάδος',
+		'ΦΑΙΣΤΟΥ.'=>'Φαιστού',
+		'ΓΟΡΤΥΝΑΣ'=>'Γόρτυνας',
+		'ΒΙΑΝΝΟΥ'=>'Βιάννου',
+	)); ?>
 
-	<div class="row">
+	<?php echo $form->error($model,'entopiotita'); ?>
+
+	<legend>Συνηπηρέτηση</legend>
+
+	<?php echo $form->labelEx($model,'synyphrethsh'); ?>
+	<!-- <?php echo $form->textField($model,'synyphrethsh',array('size'=>15,'maxlength'=>15)); ?> -->
+	<?php echo $form->dropdownList($model,'synyphrethsh',array(
+		'' => 'Επιλέξτε Δήμο',
+		'ΗΡΑΚΛΕΙΟΥ'=>'Ηρακλείου',
+		'ΜΑΛΕΒΙΖΙΟΥ'=>'Μαλεβιζίου',
+		'ΧΕΡΣΟΝΗΣΟΥ'=>'Χερσονήσου',
+		'ΑΡΧΑΝΩΝ-ΑΣ.'=>'Αρχανών - Αστερουσίων',
+		'ΜΙΝΩΑ-ΠΕΔ.'=>'Μίνωα - Πεδιάδος',
+		'ΦΑΙΣΤΟΥ.'=>'Φαιστού',
+		'ΓΟΡΤΥΝΑΣ'=>'Γόρτυνας',
+		'ΒΙΑΝΝΟΥ'=>'Βιάννου',
+	)); ?>
+	<?php echo $form->error($model,'synyphrethsh'); ?>
+
+
+
+	<legend>Σπούδες</legend>
+
 		<?php echo $form->labelEx($model,'spoudes'); ?>
 		<!-- <?php echo $form->textField($model,'spoudes',array('size'=>15,'maxlength'=>15)); ?> -->
 		<?php echo $form->dropdownList($model,'spoudes',array(
@@ -138,26 +121,9 @@
 		)); ?>
 
 		<?php echo $form->error($model,'spoudes'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'goneis_dhmos'); ?>
-		<!-- <?php echo $form->textField($model,'goneis_dhmos',array('size'=>15,'maxlength'=>15)); ?> -->
-		<?php echo $form->dropdownList($model,'synyphrethsh',array(
-			'' => 'Επιλέξτε Δήμο',
-			'ΗΡΑΚΛΕΙΟΥ'=>'Ηρακλείου',
-			'ΜΑΛΕΒΙΖΙΟΥ'=>'Μαλεβιζίου',
-			'ΧΕΡΣΟΝΗΣΟΥ'=>'Χερσονήσου',
-			'ΑΡΧΑΝΩΝ-ΑΣ.'=>'Αρχανών - Αστερουσίων',
-			'ΜΙΝΩΑ-ΠΕΔ.'=>'Μίνωα - Πεδιάδος',
-			'ΦΑΙΣΤΟΥ.'=>'Φαιστού',
-			'ΓΟΡΤΥΝΑΣ'=>'Γόρτυνας',
-			'ΒΙΑΝΝΟΥ'=>'Βιάννου',
-		)); ?>
-		<?php echo $form->error($model,'goneis_dhmos'); ?>
-	</div>
+	<legend>Λόγοι Υγείας Γονέων</legend>
 
-	<div class="row">
 		<?php echo $form->labelEx($model,'goneis_ygeia'); ?>
 		<!-- <?php echo $form->textField($model,'goneis_ygeia',array('size'=>15,'maxlength'=>15)); ?> -->
 		<?php echo $form->dropdownList($model,'goneis_ygeia',array(
@@ -166,31 +132,40 @@
 			'67% κ άνω'=>'67% και άνω',
 		)); ?>
 		<?php echo $form->error($model,'goneis_ygeia'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'protaireotita'); ?>
-		<!-- <?php echo $form->textField($model,'protaireotita',array('size'=>30,'maxlength'=>30)); ?> -->
-		<?php echo $form->dropdownList($model,'protaireotita',array(
-			'' => 'Επιλέξτε Περίπτωση',
-			'ΕΙΔΙΚΗ ΚΑΤΗΓ. ΜΕΤΑΘΕΣΗΣ'=>'Ειδική Κατηγορία Μετάθεσης',
-			'ΣΥΖΥΓΟΣ ΕΝΣΤΟΛΟΥ'=>'Σύζυγος Ενστόλου',
-			'ΣΥΖΥΓΟΣ ΔΙΚΑΣΤΙΚΟΥ'=>'Σύζυγος Δικαστικού',
-			'ΕΚΛΕΓΜΕΝΟΣ ΚΑΛΛΙΚΡΑΤΗΣ'=>'Εκλεγμένος βάση Καλλικράτη',
-		)); ?>
-		<?php echo $form->error($model,'protaireotita'); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'parathrhseis'); ?>
-		<?php echo $form->textArea($model,'parathrhseis',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'parathrhseis'); ?>
-	</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Καταχώρηση' : 'Αποθήκευση'); ?>
-	</div>
+	<legend>Λοιπά</legend>
+
+	<?php echo $form->labelEx($model,'protaireotita'); ?>
+	<!-- <?php echo $form->textField($model,'protaireotita',array('size'=>30,'maxlength'=>30)); ?> -->
+	<?php echo $form->dropdownList($model,'protaireotita',array(
+		'' => 'Επιλέξτε Περίπτωση',
+		'ΕΙΔΙΚΗ ΚΑΤΗΓ. ΜΕΤΑΘΕΣΗΣ'=>'Ειδική Κατηγορία Μετάθεσης',
+		'ΣΥΖΥΓΟΣ ΕΝΣΤΟΛΟΥ'=>'Σύζυγος Ενστόλου',
+		'ΣΥΖΥΓΟΣ ΔΙΚΑΣΤΙΚΟΥ'=>'Σύζυγος Δικαστικού',
+		'ΕΚΛΕΓΜΕΝΟΣ ΚΑΛΛΙΚΡΑΤΗΣ'=>'Εκλεγμένος βάση Καλλικράτη',
+	)); ?>
+	<?php echo $form->error($model,'protaireotita'); ?>
+
+	<?php echo $form->labelEx($model,'parathrhseis'); ?>
+	<?php echo $form->textArea($model,'parathrhseis',array('rows'=>6, 'cols'=>50)); ?>
+	<?php echo $form->error($model,'parathrhseis'); ?>
+
+</fieldset>
+
+<div class="form-actions">
+    <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'primary', 'label'=> $model->isNewRecord ? 'Καταχώρηση' : 'Αποθήκευση')); ?>
+    <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'reset', 'label'=>'Reset')); ?>
+</div>
+
+
 
 <?php $this->endWidget(); ?>
 
-</div><!-- form -->
+
+
+
+	<p class="note">Τα πεδία μαρκαρισμένα με <span class="required">*</span> είναι υποχρεωτικά.</p>
+
+	<?php echo $form->errorSummary($model); ?>
